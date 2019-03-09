91cf759aaf63fdb6a3bb51811fd1c96cff57911c
sarawutb <37133493+sarawutb@users.noreply.github.com>
2019-03-09 23:32:54 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'caLVtgWgsej7hg4QNhlp+6gwksxuzTwDm01l/k3YICFAGppKOp1Hg7Bhi3qeOcP8qUnwvfK7yYlDFDMI2qey4YaZpZO4z8+y7qp62JuHyn/JjyZYFLFFTi8X0OY899ViyXcYYwejOcbVq40Ga7nLIAdB04t89/1O/w1cDnyilFU=';
+ $access_token = 'W6j0/wKI5kGrpmHJWUjriP8Rxc8k3CwP7izdeCtIi8mNTaaNdb59Y5tLgdGl2bZoqUnwvfK7yYlDFDMI2qey4YaZpZO4z8+y7qp62JuHyn87PZ0qBjSMQ5BPH2NU+ZWSdGKd2dCjsoDMQ8vEACEUPwdB04t89/1O/w1cDnyilFU=';
 
   $messages = [
         'type' => 'text',
@@ -13,7 +13,7 @@ function send_LINE($msg){
       // Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
-        'to' => '209e5c6454a48f0b09e83cbd1016a718',
+        'to' => 'U75a113bb4a54bf53f9d6abfea0cd844f',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
