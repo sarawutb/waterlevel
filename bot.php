3e62e708b660391231ed5556687a02820873bae0
sarawutb <37133493+sarawutb@users.noreply.github.com>
2019-03-10 02:01:55 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,12 +12,14 @@ $content = file_get_contents('php://input');
 
 $events = json_decode($content, true);
 // Validate parsed JSON data
+/*
 if (!is_null($events['ESP'])) {
 	
 	send_LINE($events['ESP']);
 		
 	echo "OK";
 	}
+	*/
 
 if (!is_null($events['events'])) {
 	echo "line bot";
